dbd7ea4b6a69123f89ae1e24193a7dbcfad47685
RicardoValadez <36349018+ValadezRicardo@users.noreply.github.com>
2024-11-08 15:26:41 -0600

    add routes (app.js and navbar.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 // import Contacto from './components/Contacto.jsx'
-import Dashboard from './components/Dashboard.jsx'
+// import Dashboard from './components/Dashboard.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
